bba166ee9fb00f7ef1d41ed336112b7d1f1f166a
urvashi <uchouhan@bestpeers.com>
2018-03-20 20:25:29 +0530

    updated files

--- sud.js
+++ sud.js
@@ -1,57 +1,50 @@
-$(document).ready(function() {	
-	var x=0,j=0,k=0,i=0,z=0,k1=0,c=0;
-	var cell;
-	var v=[];
-	var v1=[];
-	var v2=[];
-	var t1;
-	var t2=[];
-	var td = [];
-	var t3=[];
+//Sudoku game program
+$(document).ready(function() {		
 	var val;
-	randomNum1 =30+ Math.round(Math.random() * 10);		
+	randomNum1 =5+ Math.round(Math.random() * 10);//creates a random no between 5 to 15 to decide what number of cells will be pre-filled	
 	console.log(randomNum1)
 	for(i=0;i<randomNum1;i++)
 	{
-		randomNum2=Math.ceil(Math.random() * 9);
-		randomNum3=Math.round(Math.random() * 80);
-		if($('#'+randomNum3).text()!=""){
+		randomNum2=Math.ceil(Math.random() * 9);   //creates a random no between 1 to 9 
+		randomNum3=Math.round(Math.random() * 80); //creates a random no between 0 to 80 for id
+		if($('#'+randomNum3).text()!=""){          //if an id is not empty
 			i--;
 			continue;
 		}
 		else{
-			var a=test(randomNum2,randomNum3)
-			if(a==1){
+			var check=test(randomNum2,randomNum3)
+			if(check==1){
 				i--;
 			}
 		}
 	}
 	
-	$('.b').click(function(){
+	$('.btn').click(function(){								//when button is clicked
 		val=$(this).val();
-		$('.tdi').on('click',function(){
+		$('.tdi').on('click',function(){				//when a cell is selected
 			var cell=$(this).attr("id")
 			test1(val,cell)			
 		})			
 	})
-  function test1(val,cell){
+  function test1(val,cell){									//function for testing user inputs for sudoku constraints
   	var className = $('#'+cell).attr('class');
-  	var table=parseInt(className.split(' ')[1].substr(1));
+  	  	var table=parseInt(className.split(' ')[1].substr(1));
 		var row=parseInt(className.split(' ')[2].substr(1));
 		var col=parseInt(className.split(' ')[3].substr(1));
 		var table1=($('.t'+table).text());
 		var col1=($('.c'+col).text());
 		var row1=($('.r'+row).text());
 		if((table1.indexOf(val) == -1)&&(col1.indexOf(val) == -1)&&(row1.indexOf(val) == -1)){
-			$('#'+cell).addClass('success');}
-		
+			$('#'+cell).empty();
+			$('#'+cell).text(val).css('color','green')			
+		}		
 		else{
-			$('#'+cell).addClass('warning');
+			$('#'+cell).empty();
+			$('#'+cell).text('X').css('color','red')				
 		}
-		$('#'+cell).text(val)		
   }
-	function test(randomNum2,randomNum3){
-		var j1=0;
+	function test(randomNum2,randomNum3){  //function for testing pre-filled values for sudoku constraints
+		var count1=0;
 		var className = $('#'+randomNum3).attr('class');
 		var table=parseInt(className.split(' ')[1].substr(1));
 		var row=parseInt(className.split(' ')[2].substr(1));
@@ -60,13 +53,13 @@ $(document).ready(function() {
 		var col1=($('.c'+col).text());
 		var row1=($('.r'+row).text());
 		if((table1.indexOf(randomNum2) == -1)&&(col1.indexOf(randomNum2) == -1)&&(row1.indexOf(randomNum2) == -1))				
-		$('#'+randomNum3).text(randomNum2)
-		else
-			++j1;	
-		return j1;
-	}
-		
+		$('#'+randomNum3).text(randomNum2).css("color","blue")
+		else{
+			++count1;	
+		}
+		return count1;
+	}		
 });
-
+	
 
 
--- sudoku1.js
+++ sudoku1.js
@@ -1,11 +1,13 @@
 
-var badNumberHint = true;
+var badNumberHint = true; 
 $(document).ready(function(){
-  a=('803070295010560003590302006000000040601000307020000000700604019100097030986020704');
+  a=('803070295010560003590302006000000040601000307020000000700604019100097030986020704');   
   b=('100489006730050040460001295387120600501703008046095710914600080020040037803512004');
   c=('200300000804062003013800200000020390507000621032006000020009140601250309000001002');
   d=('240007000189000275503001400010020007007000020950000340704002890320008054805714600');
-  randomNum=Math.ceil(Math.random() * 4);
+  e=('530070000600195000098000060800060003400803001700020006000000200000419005000080079');
+  f=('406070308057090140109408205970385004003720010680049053704602501065010900308050402');
+  randomNum=Math.ceil(Math.random() * 6);  //creates random nu from 1 to 6
  
   switch(randomNum){
     case 1:
@@ -20,11 +22,18 @@ $(document).ready(function(){
     case 4:
       initPuzzle(d);
       break;
+    case 5:
+      initPuzzle(e);
+      break;
+    case 6:
+      initPuzzle(f);
+      break;
+
   }
-  $('.cell').on('click',doClick);
-  $('.cell').on('keydown',doKeyDown);
+  $('.cell').on('click',doClick);        // function called when user clicks on a cell
+  $('.cell').on('keydown',doKeyDown);    //function called when user press a button
 });
-function doKeyDown(event) {
+function doKeyDown(event) {              //function definition of the one which gets called when user press a button
   switch (event.keyCode) {                                  
     default: 
       if (event.keyCode >= 48 && event.keyCode <= 57) {
@@ -37,14 +46,14 @@ function doKeyDown(event) {
       }
   }
 }
-function doNumber(n) {
+function doNumber(n) {          
 
   if (badNumberHint) { tryNumber(n) } else { putGoodNumber(n) }
 }
-function tryNumber(n) {
+function tryNumber(n) {           //function which passes the no to test the constraints
   if (testNumber(n)) { putGoodNumber(n) } else { putBadNumber(n) }
 }
-function putGoodNumber(n){
+function putGoodNumber(n){        //function through which color of right input turns black
   $('#'+focusedCell).removeClass('warning');
   if (n == 0) {
     $('#'+focusedCell).text('');
@@ -53,36 +62,42 @@ function putGoodNumber(n){
    
   }
 }
-function putBadNumber(n){
+function putBadNumber(n){         //function through which color of wrong input turns red
   $('#'+focusedCell).addClass('warning');
   $('#'+focusedCell).text(n);
 }
 
-function doClick(event) {
+function doClick(event) {         //function definition of the one which gets called when user clicks on a cell
   event.preventDefault();
-  focusedCell = this.id;  
+  focusedCell = this.id;
+
 }
 function doRightClick(event) {
   event.preventDefault();
   alert('doRightClick('+this.id+')');
 }
-function initPuzzle(str) {
-  var n;
+function initPuzzle(str) {        //function which gets called when the page load and prints the pre-filled values
+  var n; 
   for (i=0; i<=80; i++) {
     n = str.substr(i,1);
-    if (n >= 1 && n <= 9) { $('#'+i).text(n) } else { $('#'+i).text('') }
+
+    if (n >= 1 && n <= 9) { $('#'+i).text(n) .css("font-weight","bold")
+      v=i;
+   
+    }
+     else { $('#'+i).text('') }
   }
 }
-function boxNo(cell) {
+function boxNo(cell) {         //function to extract to which table the particular cell belongs to
   return parseInt(cell.className.split(' ')[1].substr(3));
 }
-function rowNo(cell) {
+function rowNo(cell) {         //function to extract to which row the particular cell belongs to
   return parseInt(cell.className.split(' ')[2].substr(3));
 }
-function colNo(cell) {
+function colNo(cell) {         //function to extract to which coloumn the particular cell belongs to
   return parseInt(cell.className.split(' ')[3].substr(3));
 }
-function testNumber(n) {
+function testNumber(n) {      //function to test the sudoku constraints
   var cell = document.getElementById(focusedCell);
   var box = boxNo(cell);
   var row = rowNo(cell);
