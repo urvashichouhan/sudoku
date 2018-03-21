c92663a42f7e51d329bda26ec9a1bead3c2802bb
urvashi <uchouhan@bestpeers.com>
2018-03-21 20:25:00 +0530

    updated

--- sud.js
+++ sud.js
@@ -1,6 +1,6 @@
 //Sudoku game program
 $(document).ready(function() {		
-	var val;
+	
 	randomNum1 =5+ Math.round(Math.random() * 10);//creates a random no between 5 to 15 to decide what number of cells will be pre-filled	
 	console.log(randomNum1)
 	for(i=0;i<randomNum1;i++)
@@ -19,16 +19,20 @@ $(document).ready(function() {
 		}
 	}
 	
-	$('.btn').click(function(){								//when button is clicked
+	$('.btn').click(function(){	
+	var val=null;							//when button is clicked
 		val=$(this).val();
+		fun(val)	
+	});
+	function fun(val){
 		$('.tdi').on('click',function(){				//when a cell is selected
-			var cell=$(this).attr("id")
-			test1(val,cell)			
-		})			
-	})
+			var cell=$(this).attr("id");
+			test1(val,cell);			
+		});		
+	};
   function test1(val,cell){									//function for testing user inputs for sudoku constraints
   	var className = $('#'+cell).attr('class');
-  	  	var table=parseInt(className.split(' ')[1].substr(1));
+  	var table=parseInt(className.split(' ')[1].substr(1));
 		var row=parseInt(className.split(' ')[2].substr(1));
 		var col=parseInt(className.split(' ')[3].substr(1));
 		var table1=($('.t'+table).text());
@@ -36,11 +40,11 @@ $(document).ready(function() {
 		var row1=($('.r'+row).text());
 		if((table1.indexOf(val) == -1)&&(col1.indexOf(val) == -1)&&(row1.indexOf(val) == -1)){
 			$('#'+cell).empty();
-			$('#'+cell).text(val).css('color','green')			
+			$('#'+cell).text(val).css('color','green');			
 		}		
 		else{
 			$('#'+cell).empty();
-			$('#'+cell).text('X').css('color','red')				
+			$('#'+cell).text('X').css('color','red');			
 		}
   }
 	function test(randomNum2,randomNum3){  //function for testing pre-filled values for sudoku constraints
